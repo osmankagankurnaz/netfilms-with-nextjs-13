584175384a1b97af5379afa5891890bdb83f5b6a
Osman Kagan Kurnaz <osmansaidkurnaz@gmail.com>
2023-07-29 16:55:13 +0300

    created movie-error-page

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,21 +1,24 @@
 import React from 'react'
-import {notFound} from 'next/navigation';
+import { notFound } from 'next/navigation';
 
 import MovieContainer from '@/containers/movie';
 import Movies from '@/mocks/movies.json'
 
 
-function MoviePage({ params }) {
+function MoviePage({ params, searchParams }) {
     const movieDetail = Movies.results.find(movie => movie.id.toString() === params.id)
-    console.log(params)
 
     if (!movieDetail) {
         notFound();
-      }
+    };
+
+    if (searchParams.error === "true") {
+        throw new Error("Something went wrong!");
+    };
 
     return (
-    <MovieContainer movie={movieDetail}/>
-  )
+        <MovieContainer movie={movieDetail} />
+    )
 }
 
 export default MoviePage
